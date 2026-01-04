e904e1ef2b260a8af3a2133053639a6b079c0f74
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-01-05 01:19:59 +0500

    Add production build support and update server config

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,21 +3,33 @@ import dotenv from "dotenv";
 import chatRoutes from "./routes/chatRoutes.js";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import path from "path";
 
 dotenv.config();
 
-
 const app = express();
-app.use(cors());
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors());
+}
 app.use(express.json());
 
-const PORT = process.env.PORT || 5001;
 
-app.get("/", (req, res) => {
-  res.send("This is the initial backend server for ChatZee");
-});
+// app.get("/", (req, res) => {
+//   res.send("This is the initial backend server for ChatZee");
+// });
 
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(()=>{
   app.listen(PORT, () => {
